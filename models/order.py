4baa9ab013e4f0f8854ac092f918c4bc54d1cc58
mark <marklca@hotmail.com>
2024-03-24 04:14:05 +1100

    Order model added

---
+++ models/order.py
@@ -0,0 +1,24 @@
+from init import db, ma 
+from marshmallow import fields
+
+class Order(db.Model):
+    __tablename__ = "orders"
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    message = db.Column(db.text)
+    status = db.Column(db.String)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    user = db.relationship('User', back_populates='orders')
+
+class OrderSchema(ma.Schema):
+
+    user = fields.Nested('UserSchema', only = ['email'])
+    
+    class Meta:
+        fields = ('id', 'date', 'message', 'status', 'user1')
+
+card_schema = OrderSchema()
+cards_schema = OrderSchema(many=True)
--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
-from init import db, ma 
+from init import db, ma
+from marshmallow import fields 
 
 class User(db.Model):
     __tablename__= "users"
@@ -8,10 +9,15 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
+    orders = db.relationship('Order', back_populates='user', cascade='all, delete')
+
 
 #Schema to deserialize
     
 class UserSchema(ma.Schema):
+
+    orders = fields.Nested('CardSchema', exclude=['User'])
+
     class Meta:
         fields = ('id', 'email', 'password', 'is_admin')
 
