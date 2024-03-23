190955030215f8d36ae1c1441de27cfdbb5a03d8
mark <marklca@hotmail.com>
2024-03-24 03:24:35 +1100

    Authorisation  added

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,6 +1,12 @@
+from datetime import timedelta
 from flask import Blueprint, request
 from init import db, bcrypt
 from models.user import User, user_schema
+from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import create_access_token
+from psycopg2 import errorcodes
+
+
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -8,15 +14,14 @@ auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 def auth_register():
     try:
         body_data = request.get_json()
-        password = body_data.get('password')
-        hashed_password = ""
-        if password:
-            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-        
+
         user = User(
-            email=body_data.get('email'),
-            password=hashed_password
+            email=body_data.get('email')
         )
+
+        password = body_data.get('password')
+        if password:
+            user.password=bcrypt.generate_password_hash(password).decode('utf-8')
 
         db.session.add(user)
         db.session.commit()
@@ -24,6 +29,25 @@ def auth_register():
         return user_schema.dump(user), 201
             
 
-    except Exception as e:
-        raise e
+    except IntegrityError as err:
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            return {"error": f"The {err.orig.diag.column_name} is required"}
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+            return {"error": "Email already exists"}, 409
         
+
+#create jwt and token
+
+@auth_bp.route("/login", methods=["POST"])
+def auth_login():
+
+    body_data = request.get_json()
+
+    stmt = db.select(User).filter_by(email=body_data.get("email"))
+    user = db.session.scalar(stmt)
+    if user and bcrypt.check_password_hash(user.password, body_data.get("password")):
+        token = create_access_token(identity=str(user.id), expires_delta=timedelta(days=1))
+        return {"email": user.email, "token": token, "is_admin": user.is_admin}        
+    
+    else:
+        return {"error": "Invalid email or password"}, 401
